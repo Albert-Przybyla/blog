a16bfb2173844cf8c1d4bfacc2790359921fe299
Albert-Przybyla <albert.przybyla2@gmail.com>
2022-11-01 18:12:04 +0100

    generate the basis of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { SearchComponent } from './search/search.component';
+import { MainComponent } from './main/main.component';
+import { ArticleComponent } from './article/article.component';
+import { ModalComponent } from './modal/modal.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register/register.component';
+import { CommentsComponent } from './comments/comments.component';
+import { AddComponent } from './comments/add/add.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavbarComponent, SearchComponent, MainComponent, ArticleComponent, ModalComponent, AdminComponent, LoginComponent, RegisterComponent, CommentsComponent, AddComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
